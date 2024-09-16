f4d303984c3f32669d4e2709675246acc7b40067
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-09-17 00:43:04 +0530

    finished

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ image2 = Image.open('2.jpg')
 
 hash1=imagehash.average_hash(image1)
 hash2=imagehash.average_hash(image2)
-cutoff = 5
+cutoff = 30
 
 
 if hash1 - hash2 < cutoff:
--- main1.py
+++ main1.py
@@ -2,3 +2,10 @@ from PIL import Image,ImageChops
 
 img1 = Image.open("1.jpg")
 img2 = Image.open("2.jpg")
+
+# image chanels operations containd calucation methods for images
+
+diff=ImageChops.difference(img1,img2)
+
+if diff.getbbox():
+    diff.show()
